dd5636eb95cde8a00f8bbe993f372b441fff1884
Winston Nji <winston00russel@gmail.com>
2025-05-23 18:16:56 +0200

    Loading.tsx fallback files implemented for single page and order page

---
+++ restaurant_app_next_js/src/app/MenuItem/[id]/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-white z-50">
+      <h1 className="text-3xl text-red-400 font-bold">Loading Item...</h1>
+    </div>
+  )
+}
---
+++ restaurant_app_next_js/src/app/OrderPage/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-red-200 z-50">
+      <h1 className="text-3xl text-red-400 font-bold">Loading Cart Items...</h1>
+    </div>
+  )
+}

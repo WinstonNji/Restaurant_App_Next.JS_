101d0dd90ef7479aa0ae3eb5e1083bea68dfab3d
Winston Nji <winston00russel@gmail.com>
2025-05-23 18:17:32 +0200

    Iplemented Suspense fallback for menu Featured section  for landing page

--- restaurant_app_next_js/src/app/page.tsx
+++ restaurant_app_next_js/src/app/page.tsx
@@ -1,13 +1,30 @@
 import Slider from '@/components/Slider'
 import Featured from '@/components/Featured'
 import Offer from "@/components/Offer"
+import {PrefetchModalTree} from "@/components/PrefetchModalTree"
+import { Suspense } from 'react'
 
 export default function Home() {
   return (
     <>
       <Slider />
-      <Featured />
+      <Suspense fallback = {
+        <div className='flex gap-4'>
+          {[...Array(9)].map((_,index) => (
+            <div key={index} className="flex w-52 h-full flex-col gap-4 min-w-[80vw] md:min-w-[30vw]">
+              <div className="skeleton h-32 w-full"></div>
+              <div className="skeleton h-4 w-28"></div>
+              <div className="skeleton h-4 w-full"></div>
+              <div className="skeleton h-4 w-full"></div>
+            </div>
+          ))}
+        </div>
+        
+      }>
+        <Featured />
+      </Suspense>
       <Offer />
+      <PrefetchModalTree />
     </>
     
   );
